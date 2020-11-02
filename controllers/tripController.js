5ff0308c1b7109ca729860aafc938e847c0cd168
reng <reng412@gmail.com>
2020-11-01 19:06:25 -0800

    Update trip post route to post to restaurants

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 // Import the model (trip.js) to use its database functions.
 const db = require("../models/index.js");
+const trip = require("../models/trip.js");
 
 // Create all our routes and set up logic within those routes where required.
 
@@ -27,17 +28,37 @@ router.get("/api/trips", function (req, res) {
     })
 });
 
+// Route for creating a new trip which includes restaurants & sights
 router.post("/api/trips", function (req, res) {
     db.Trip.create(req.body)
         .then(function (trip) {
-            // Send back the ID of the new user
-            console.log(trip);
-            res.json(trip)
+            // Add trip restaurants to restaurant
+            db.TripRestaurant.create({
+                RestaurantId: req.body.restaurantId,
+                TripId: trip.id
+            }).then(function (tripRest) {
+                console.log(tripRest)
+                db.TripSight.create({
+                    SightId: req.body.restaurantId,
+                    TripId: trip.id
+                })
+            }).then(function () {
+                res.send(true);
+            })
         }).catch(function (err) {
             console.log(err);
             res.send(false);
         })
 });
+
+// Example post request
+// {
+//     "name":"My London Trip",
+//     "description": "Trip to London Town",
+//     "CityId": 3,
+//     "UserId": 2,
+//     "restaurantId": 8
+//  }
 
 
 // Export routes for server.js to use.
