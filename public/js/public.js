48444599a42ea7933a0c1033f6d5910d3ae313cb
Kelly Stone <kellystone916@gmail.com>
2020-11-02 23:05:06 -0800

    EVERYTHING IS WORKINGgit add .

--- public/js/public.js
+++ public/js/public.js
@@ -1,27 +1,44 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-//ADD CITY TO SAVED LIST on CITY.HTML & SAVED.HTML
-$(function(){
-
+$(document).on("ready", function(){
 $(".saveTripBtn").on("click", function(event){
+  const selectedRestaurants = [];
+  const selectedSights = [];
+  $("#userSelectRestaurant :selected").each((i, selectRestaurant) => {
+    if (i > 0) {
+      selectedRestaurants.push($(selectRestaurant).val())
+    }
+  })
+  $("#userSelectSight :selected").map((i, selectSight) => {
+    if (i > 0) {
+      selectedSights.push($(selectSight).val());
+    }
+  })
   event.preventDefault();
+  // const url = window.location.href.split("/")
+  // const cityId = url[url.length -1]
   var newTrip = {
-      name: $(".savedUserName").val(),
+      name: $(".userTripName").val(),
+      UserId: $(".savedUserName :selected").val(),
       description: $(".userWrittenDescription").val(),
-      RestaurantId: [1],
-      SightId: [1]
-    }      
-    $.ajax("/api/trips", {
-      type: "POST",
-      data: newTrip
-    }).then(
+      restaurantId: selectedRestaurants,
+      sightId: selectedSights,
+      CityId : $(this).data("id")
+    }  
+      
+    console.log(newTrip)
+    // console.log(newTrip);
+
+     $.ajax("/api/trips", {
+       type: "POST",
+       data: newTrip
+     }).then(
       function() {
-        console.log("added New Trip");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-  $(".signUpBtn").on("click", function(event){
+         console.log("added New Trip");
+         // Reload the page to get the updated list
+         location.reload();
+       }
+     );
+});
+$(".signUpBtn").on("click", function(event){
     event.preventDefault();
     var newUser = {
         name: $(".savedName").val()
@@ -37,21 +54,18 @@ $(".saveTripBtn").on("click", function(event){
         }
       );
     });
-function saveTrip(){
-    $.ajax("/api/trips", {
-      type: "GET",
-    }).then(
-      function() {
-        console.log("added New Trip");
-        // Reload the page to get the updated list
+// function saveTrip(){
+//     $.ajax("/api/trips", {
+//       type: "GET",
+//     }).then(
+//       function() {
+//         console.log("added New Trip");
+//         // Reload the page to get the updated list
         
-      }
+//       }
 
-    );
-}
-var saved = ($(".savedName").val());  
-console.log(saved);
-
-  saveTrip();
+//     );
+// }
 });
+
 
